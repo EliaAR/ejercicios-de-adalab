a65e040b0f5a4d6d1a5832f68021e48c4e1743c0
EliaAR <alonsoramos.elia@gmail.com>
2021-05-11 19:45:47 +0200

    fix exercise 8 chapter 7

--- modulo-2-leccion-07-ejercicios/index.js
+++ modulo-2-leccion-07-ejercicios/index.js
@@ -69,8 +69,8 @@ const corp = document.querySelector(".js-container");
 function handlerButton() {
   corp.classList.toggle("js-background");
 }
-classes[0].addEventListener("click", handlerButton);
-classes[1].addEventListener("click", handlerButton);
-classes[2].addEventListener("click", handlerButton);
+for (let i = 0; i < classes.length; i++) {
+  classes[i].addEventListener("click", handlerButton);
+}
 
 //Ejercicio 10
