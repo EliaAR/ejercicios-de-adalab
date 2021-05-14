3e5ba3813e8791b24bdd174250f25db2e60f5102
EliaAR <alonsoramos.elia@gmail.com>
2021-05-14 14:41:00 +0200

    add exercise 7 chapter 9

--- modulo-2-leccion-09-ejercicios-Bonus/main.js
+++ modulo-2-leccion-09-ejercicios-Bonus/main.js
@@ -1,10 +1,10 @@
 //Ejercicio 5
 let character = "▲";
 function tree1(heightTree) {
-  for (i = 0; i < heightTree; i++) {
+  for (let i = 0; i < heightTree; i++) {
     let lineTree = "";
     let parameterNumber = i + 1;
-    for (j = 0; j < parameterNumber; j++) {
+    for (let j = 0; j < parameterNumber; j++) {
       lineTree = lineTree + character;
     }
     console.log(lineTree);
@@ -22,3 +22,32 @@ let verticalLine = "|";
 console.log(start);
 tree1(5);
 console.log(verticalLine);
+
+//Separación
+console.log("");
+console.log("");
+
+//Ejercicio 7
+let empty = " ";
+function tree2(heightTree) {
+  let lineStart = "";
+  let LineStartWidth = heightTree - 1;
+  for (let z = 0; z < LineStartWidth; z++) {
+    lineStart += empty;
+  }
+  console.log(`${lineStart}${start}`);
+  for (let i = 0; i < heightTree; i++) {
+    let lineTree = "";
+    let lineCharacterWidth = i * 2 + 1;
+    let LineEmptyWidth = heightTree - 1 - i;
+    for (let j = 0; j < LineEmptyWidth; j++) {
+      lineTree = lineTree + empty;
+    }
+    for (let k = 0; k < lineCharacterWidth; k++) {
+      lineTree = lineTree + character;
+    }
+    console.log(lineTree);
+  }
+  console.log(`${lineStart}${verticalLine}`);
+}
+tree2(5);
