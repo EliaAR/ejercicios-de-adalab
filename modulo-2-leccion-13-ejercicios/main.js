dc0aae4f3b2896944917c5c80008ce3a5b8ef40f
EliaAR <alonsoramos.elia@gmail.com>
2021-05-19 20:14:09 +0200

    add exercise 2 chapter 13

--- modulo-2-leccion-13-ejercicios/main.js
+++ modulo-2-leccion-13-ejercicios/main.js
@@ -3,3 +3,8 @@ let marks = [5, 4, 6, 7, 9];
 let inflatedMarks = marks.map((mark) => mark + 1);
 
 console.log(inflatedMarks);
+
+//Ejercicio 2
+let names = ["María", "Lucía", "Susana", "Rocío", "Inmaculada"];
+let hello = names.map((name) => `Bienvenida ${name}`);
+console.log(hello);
