e20b616c04b8e7ef2c1bf66db572547e6132cfce
EliaAR <alonsoramos.elia@gmail.com>
2021-05-19 20:26:33 +0200

    add exercise 3 chapter 13

--- modulo-2-leccion-13-ejercicios/main.js
+++ modulo-2-leccion-13-ejercicios/main.js
@@ -8,3 +8,20 @@ console.log(inflatedMarks);
 let names = ["María", "Lucía", "Susana", "Rocío", "Inmaculada"];
 let hello = names.map((name) => `Bienvenida ${name}`);
 console.log(hello);
+
+//Ejercicio 3
+const users = [
+  { name: "María", isPremium: false },
+  { name: "Lucía", isPremium: true },
+  { name: "Susana", isPremium: true },
+  { name: "Rocío", isPremium: false },
+  { name: "Inmaculada", isPremium: false },
+];
+let hi = users.map((user) => {
+  if (user.isPremium) {
+    return `Bienvenida ${user.name}.Gracias por confiar en nosotras`;
+  } else {
+    return `Bienvenida ${user.name} ¿Aún no eres Premium?`;
+  }
+});
+console.log(hi);
