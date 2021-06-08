94e9dd40184e54a6ce81ac96634e6c58d3d95123
EliaAR <alonsoramos.elia@gmail.com>
2021-06-08 20:21:25 +0200

    add exercise 3 chapter 7

--- modulo-3/modulo-3-leccion-7-ejercicios/src/form.jsx
+++ modulo-3/modulo-3-leccion-7-ejercicios/src/form.jsx
@@ -5,6 +5,8 @@ function Form() {
   const [text, setText] = useState("");
   const [textarea, setTextarea] = useState("");
   const [select, setSelect] = useState("Español");
+  const [radio, setRadio] = useState("todos los públicos");
+  const [checkbox, setCheckbox] = useState("comedia");
   return (
     <>
       <form className="form">
@@ -28,10 +30,35 @@ function Form() {
           <option value="Inglés">Inglés</option>
           <option value="Portugués">Portugués</option>
         </select>
+        <fieldset
+          className="for__input"
+          value={radio}
+          onchange={(evt) => setRadio(evt.currentTarget.value)}
+        >
+          <label htmlFor="todos">Todos los públicos</label>
+          <input type="radio" value="todos los públicos" id="todos" />
+          <label htmlFor="12">12 años</label>
+          <input type="radio" value="12" id="12" />
+          <label htmlFor="18">Mayor de edad</label>
+          <input type="radio" value="18" id="18" />
+        </fieldset>
+        <fieldset
+          value={checkbox}
+          onchange={(evt) => setCheckbox(evt.currentTarget.value)}
+        >
+          <label htmlFor="comedia">Comedia</label>
+          <input type="checkbox" id="comedia" />
+          <label htmlFor="fantasía">Fantasía</label>
+          <input type="checkbox" id="fantasía" />
+          <label htmlFor="terror">Terror</label>
+          <input type="checkbox" id="terror" />
+        </fieldset>
       </form>
       <p>{text}</p>
       <p>{textarea}</p>
       <p>{select}</p>
+      <p>{radio}</p>
+      <p>{checkbox}</p>
     </>
   );
 }
